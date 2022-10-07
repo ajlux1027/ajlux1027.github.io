2cdf85dc2a3e129d7ce33843263fa6d2d0c98855
ajlux1027 <112489552+ajlux1027@users.noreply.github.com>
2022-10-06 21:07:51 -0400

    Update assignment03.js

--- assignments/assignment03/assignment03.js
+++ assignments/assignment03/assignment03.js
@@ -1,1 +1,13 @@
-
+//The function for 3B
+function GCDFinder() {
+   var numOne = parseInt(document.getElementById("inputNumOne").value);
+   var numTwo = parseInt(document.getElementById("inputNumTwo").value);
+   var Gcd = 1;
+  while(numTwo) {
+    Gcd = numTwo;
+    numTwo = numOne % numTwo;
+    numOne = Gcd;
+  }
+document.getElementById("output").innerHTML= Gcd;
+  }
+}
