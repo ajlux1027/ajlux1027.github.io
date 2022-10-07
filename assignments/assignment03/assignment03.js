fc0e472d3e11c30bca6eb7e70d94d34d6a7402be
ajlux1027 <112489552+ajlux1027@users.noreply.github.com>
2022-10-06 23:14:28 -0400

    Update assignment03.js

--- assignments/assignment03/assignment03.js
+++ assignments/assignment03/assignment03.js
@@ -13,12 +13,12 @@ document.getElementById("output").innerHTML= Gcd;
 
 //the function for 3C
 function AddDigits(){
-var value = parseInt(document.getElementById("inputNum").value);,
+var value = parseInt(document.getElementById("inputNum").value);
     sum = 0;
 
 while (value) {
     sum += value % 10;
     value = Math.floor(value / 10);
-}
-document.getElementById("total").innerHTML= value;
+ }
+document.getElementById("total").innerHTML= sum;
 }
