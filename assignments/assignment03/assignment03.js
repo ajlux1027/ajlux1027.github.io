dfc0380c2bbf95c3e9f9991367c4144d547dcf0a
ajlux1027 <112489552+ajlux1027@users.noreply.github.com>
2022-10-14 23:20:19 -0400

    Update assignment03.js

--- assignments/assignment03/assignment03.js
+++ assignments/assignment03/assignment03.js
@@ -51,10 +51,11 @@ function ticTac(userSelection) {
         return;
     console.log("turn  is " + turn);
     //User Moves
-    let xo = 'X';
+    let character = 'X';
     let input = 0;
-    if(turn % 2 == 0) {	//Checks if turn count is even or odd for user
-        xo = 'X';
+    if(turn % 2 == 0) {	
+    //Checks if turn count is even or odd for user
+        character = 'X';
         			
 		 
 
@@ -68,10 +69,12 @@ function ticTac(userSelection) {
 
         console.log("input =" + input)
         if(input >= 0 && input <= 8) {
-            if (boxes[input].innerText == 'X' || boxes[input].innerText == 'O') // decides if spot is already taken
+            if (boxes[input].innerText == 'X' || boxes[input].innerText == 'O') 
+            // decides if spot is already taken
                  turn--;
             else {
-                boxes[input].innerText = xo;		// If spot is not taken, X/O placed in position
+                boxes[input].innerText = character;		
+                // If spot is not taken, X/O placed in position
                 boxes[input].style.backgroundColor = "antiquewhite";
             }
                 
@@ -80,34 +83,35 @@ function ticTac(userSelection) {
             console.log(userSelection)
         }
         turn++;
-        /* console.log("Turn = " + turn);
-        console.log();
-        console.log();
-        console.log(); */
         if(winState() || turn == 9)
             return;
         }
         if(turn % 2 == 1) {
             console.log("Robot is Making its move");
             let temporary = -1;
-            xo = 'O';		//O placed on odd turns
+            character = 'O';		
+            //O placed on odd turns
             input = -1;
-        	input = ticTacBrain('O');        //Possible wins checked first
+        	input = ticTacBrain('O');        
+            //Possible wins checked first
             console.log("Robot input is " + input);
-       		temporary = ticTacBrain('X');    //Defensive moves checked if no wins present
+       		temporary = ticTacBrain('X');    
+            //Defensive moves checked if no wins present
             console.log("Robot temp is " + temporary);
        		
        		if(input == -1 && temporary > -1) input = temporary;
            
-            if(input == -1) input = ticTacBackup(); //Advacned strategy accessed if no drasic moves exist
+            if(input == -1) input = ticTacBackup(); 
+            //Advacned strategy accessed if no drasic moves exist
             
             
             while(input == -1) {
-            	input = Math.floor(Math.random() * 9);         //Last resort random #
+            	input = Math.floor(Math.random() * 9);         
+		    //Last resort random #
                 if(boxes[input].innerText != '') input = -1;
 			} 
     		
-        boxes[input].innerText = xo;
+        boxes[input].innerText = character;
         boxes[input].style.color = "#2C68FF";
         boxes[input].style.backgroundColor = "antiquewhite";
 		turn++;
@@ -115,7 +119,8 @@ function ticTac(userSelection) {
 
         //Switch to check if a spot is open. Called whenever any player attempts a move
         
-        winState();			//Check for possible win after each move
+        winState();			
+        //Check for possible win after each move
 
 }
 function ticTacBrain(spotlight) {
@@ -177,66 +182,78 @@ function ticTacBackup() {
     else return -1;
 }
 
-function winState() {	//Compares current inputs. All win conditions independently checked
+function winState() {	
+//Compares current inputs. All win conditions independently checked
     
-    if((boxes[0].innerText == boxes[3].innerText) && (boxes[3].innerText == boxes[6].innerText))  		      //Win on Left Column
+    if((boxes[0].innerText == boxes[3].innerText) && (boxes[3].innerText == boxes[6].innerText))  		      
+    //Win on Left Column
         if(declareWinner(boxes[0].innerText)) {
             showWin(boxes[0], boxes[3], boxes[6]);
             return true;
         }								  
     
-    if((boxes[1].innerText == boxes[4].innerText) && (boxes[4].innerText == boxes[7].innerText)) 		  //Win on Middle Column
+    if((boxes[1].innerText == boxes[4].innerText) && (boxes[4].innerText == boxes[7].innerText)) 		  
+    //Win on Middle Column
         if(declareWinner(boxes[1].innerText)) {
             showWin(boxes[1], boxes[4], boxes[7]);
             return true;
         }
 
-    if((boxes[2].innerText == boxes[5].innerText) && (boxes[5].innerText == boxes[8].innerText)) 	      //Win on Right Column
+    if((boxes[2].innerText == boxes[5].innerText) && (boxes[5].innerText == boxes[8].innerText)) 	      
+    //Win on Right Column
         if(declareWinner(boxes[2].innerText)) {
             showWin(boxes[2], boxes[5], boxes[8]);
             return true;
         }
     
-    if((boxes[0].innerText == boxes[1].innerText) && (boxes[1].innerText == boxes[2].innerText))		  //Win on Top Row
+    if((boxes[0].innerText == boxes[1].innerText) && (boxes[1].innerText == boxes[2].innerText))		  
+    //Win on Top Row
         if(declareWinner(boxes[0].innerText)) {
             showWin(boxes[0], boxes[1], boxes[2]);
             return true;
         }
     
-    if((boxes[3].innerText == boxes[4].innerText) && (boxes[4].innerText == boxes[5].innerText))         //Win on Middle Row
+    if((boxes[3].innerText == boxes[4].innerText) && (boxes[4].innerText == boxes[5].innerText))         
+    //Win on Middle Row
         if(declareWinner(boxes[3].innerText)) {
             showWin(boxes[3], boxes[4], boxes[5]);
             return true;
         }
 
-    if((boxes[6].innerText == boxes[7].innerText) && (boxes[7].innerText == boxes[8].innerText)) 	      //Win on Bottom row
+    if((boxes[6].innerText == boxes[7].innerText) && (boxes[7].innerText == boxes[8].innerText)) 	      
+    //Win on Bottom row
         if(declareWinner(boxes[6].innerText)) {
             showWin(boxes[6], boxes[7], boxes[8]);
             return true;
         }
     
-    if((boxes[0].innerText == boxes[4].innerText) && (boxes[4].innerText == boxes[8].innerText)) 	 	//Win On '\' Diagonal
+    if((boxes[0].innerText == boxes[4].innerText) && (boxes[4].innerText == boxes[8].innerText)) 	 	
+    //Win On '\' Diagonal
         if(declareWinner(boxes[0].innerText)) {
         showWin(boxes[0], boxes[4], boxes[8]);
         return true;
     }
     
-    if((boxes[2].innerText == boxes[4].innerText) && (boxes[4].innerText == boxes[6].innerText))		//Win on '/' Diagonal
+    if((boxes[2].innerText == boxes[4].innerText) && (boxes[4].innerText == boxes[6].innerText))		
+    //Win on '/' Diagonal
         if(declareWinner(boxes[2].innerText)) {
             showWin(boxes[2], boxes[4], boxes[6]);
             return true;
         }
         
-    if(turn == (9)) {											//Tie game declared when all spaces filled without full row/column/diagonal
+    if(turn == (9)) {											
+	    //Tie game declared when all spaces filled without full row/column/diagonal
     	console.log("tie game")
         document.getElementById("result").innerHTML = "Tie Game!";
-    	input = 0;													//end game
+    	input = 0;													
+	    //end game
     	return true;
 	}
     return false;
 }
 
-function declareWinner(val) {		                              //Displays the Winner and the final board
+function declareWinner(val) {		                              
+	//Displays the Winner and the final board
     switch(val) 
     {
         case 'X': 
@@ -250,7 +267,9 @@ function declareWinner(val) {		                              //Displays the Winn
         case '':
             return false;
     }
-    input = 0;                              // end game
+    
+    input = 0;                              
+    // end game
     return true;
 }
 
