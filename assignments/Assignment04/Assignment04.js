8bbf816e0afa53271e86cd76100be814ea1fc91e
ajlux1027 <112489552+ajlux1027@users.noreply.github.com>
2022-11-14 21:47:03 -0500

    Update Assignment04.js

--- assignments/Assignment04/Assignment04.js
+++ assignments/Assignment04/Assignment04.js
@@ -1,1 +1,40 @@
+//make an array of random numbers between 100 and 1000
+const Arr1 = [];
+for(var i = 0; i < 11; i++){
+  Arr1[i] =100 + Math.floor(Math.random()*1001);
+}
 
+//make an array of random numbers between -200 and 200
+const Arr2 = [];
+  for(var i = 0; i < 11; i++){
+    Arr2[i] = -200 + Math.floor(Math.random()*200)
+        }
+//make an array of objects filled with objects(fname, lname, title, suffix, age.) 
+const Arr3 = [
+   {firstName:"John", 
+    lastName:"Doe", 
+    title: "Mr. ",
+    suffix: "jr. ",
+    age:46},
+  {firstName:"Jane", 
+    lastName:"Doe", 
+    title: "Ms. ",
+    suffix: "III",
+    age:16},
+  {firstName:"Ryan", 
+    lastName:"Willett", 
+    title: "Mr. ",
+    suffix: "",
+    age:22},
+  {firstName:"Alex", 
+    lastName:"Lux", 
+    title: "Ms. ",
+    suffix: "",
+    age:19},
+  {firstName:"Betsy", 
+    lastName:"Jones", 
+    title: "Mrs. ",
+    suffix: "Sr. ",
+    age:65},
+  
+]
