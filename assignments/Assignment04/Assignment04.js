4876a844f504d0270075fe774956dd723b37268d
ajlux1027 <112489552+ajlux1027@users.noreply.github.com>
2022-11-17 08:44:13 -0500

    Update Assignment04.js

--- assignments/Assignment04/Assignment04.js
+++ assignments/Assignment04/Assignment04.js
@@ -65,3 +65,40 @@ document
   .getElementById("loadnextcountry")
   .addEventListener("click", loadnextcountry);
 
+
+let country = 0
+let json
+function loadJson() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange 
+    = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("coviddata").innerHTML 
+            = "JSON data loaded.";
+          json = JSON.parse(this.responseText);
+        }
+      };
+  xhttp.open("GET", "https://api.covid19api.com/summary", true);
+  xhttp.send();
+}
+
+function loadpriorcountry () {
+  country--
+  if (country < 0) 
+    country = 0
+  document.getElementById("coviddata").innerHTML 
+            = json.Countries[country].Country
+            + ", " 
+            + json.Countries[country].TotalDeaths
+}
+
+function loadnextcountry () {
+  country++
+  if (country > json.Countries.length - 1) 
+    country = json.Countries.length - 1
+  document.getElementById("coviddata").innerHTML 
+            = json.Countries[country].Country
+            + ", " 
+            + json.Countries[country].TotalDeaths
+}
+
