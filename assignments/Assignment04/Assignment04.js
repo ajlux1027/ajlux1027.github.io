a3e120dd442f15dc068b71af225cf2789aaac455
ajlux1027 <112489552+ajlux1027@users.noreply.github.com>
2022-11-17 21:38:21 -0500

    Update Assignment04.js

--- assignments/Assignment04/Assignment04.js
+++ assignments/Assignment04/Assignment04.js
@@ -95,9 +95,16 @@ let fallTime = (distance) => {
 }
 
 
-//
+//event listener for button press to get sum of the leap years
+document
+  .getElementByName("LeapButton")
+  .addEventListener("click", SumOfLeaps);
 
-
+function SumOfLeaps(years){
+  let sum = 0
+  
+  sum += (years.map(
+}
 
 
 
